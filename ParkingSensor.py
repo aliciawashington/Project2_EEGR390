00e6678ab09e2faf6a5e5e4335e9130ee5809b08
Alicia Washington <alwas5@morgan.edu>
2020-04-19 20:57:09 -0400

    Added code for switch and functionality for LED indicators

--- ParkingSensor.py
+++ ParkingSensor.py
@@ -1,26 +1,32 @@
-from gpiozero import DistanceSensor,LED
+from gpiozero import DistanceSensor,LED,Button
 import time
 
+#Initializaiton of variables
 TRIG = 17
 ECHO = 27
-
-sensor = DistanceSensor(ECHO,TRIG,threshold_distance=0.5)
-
 redled = LED(2)
 yellowled = LED(3)
 greenled = LED(4)
-redled.on()
-time.sleep(2)
-redled.off()
-yellowled.on()
-time.sleep(2)
-yellowled.off()
-greenled.on()
-time.sleep(2)
-greenled.off()
+state = Button(0, active_state=False)
+sensor = DistanceSensor(ECHO,TRIG)
 
 while True:
+
+    if state.value() == 0:#The parking space is not reserved (Green LED should be HIGH)
+        greenled.on()
+        yellowled.off()
+        if sensor.distance <= 0.5:
+            redled.on()
+            greenled.off()
+            yellowled.off()
+    if state.value == 1: #The parking space is reserved (Yellow LED should be HIGH)
+        greenled.off()
+        yellowled.on()
+        if sensor.distance <= 0.5:
+            redled.on()
+            greenled.off()
+            yellowled.off()
+
+    time.sleep(1)
+    redled.off()
     print('Distance to nearest object is ', sensor.distance, 'm')
-
-
-
