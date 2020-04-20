68e64e11853366b9abdb0effa26eabd0254abdb6
Alicia Washington <alwas5@morgan.edu>
2020-04-19 22:58:07 -0400

    Added functionality: exit when there is keyboard interruption from user

--- ParkingSensor.py
+++ ParkingSensor.py
@@ -9,24 +9,23 @@ yellowled = LED(3)
 greenled = LED(4)
 state = Button(0, active_state=False)
 sensor = DistanceSensor(ECHO,TRIG)
-
-while True:
-
-    if state.value() == 0:#The parking space is not reserved (Green LED should be HIGH)
-        greenled.on()
-        yellowled.off()
-        if sensor.distance <= 0.5:
-            redled.on()
-            greenled.off()
+try:
+    while True:
+        if state.value() == 0:#The parking space is not reserved (Green LED should be HIGH)
+            greenled.on()
             yellowled.off()
-    if state.value == 1: #The parking space is reserved (Yellow LED should be HIGH)
-        greenled.off()
-        yellowled.on()
-        if sensor.distance <= 0.5:
-            redled.on()
+            if sensor.distance <= 0.5:
+                redled.on()
+                greenled.off()
+                yellowled.off()
+        if state.value == 1: #The parking space is reserved (Yellow LED should be HIGH)
             greenled.off()
-            yellowled.off()
-
-    time.sleep(1)
-    redled.off()
-    print('Distance to nearest object is ', sensor.distance, 'm')
+            yellowled.on()
+            if sensor.distance <= 0.5:
+                redled.on()
+                greenled.off()
+                yellowled.off()
+        redled.off()
+        print('Distance to nearest object is ', sensor.distance, 'm')
+except KeyboardInterrupt:
+    print("Measurement stopped by User")
--- ParkingSensorv2.py
+++ ParkingSensorv2.py
@@ -18,35 +18,36 @@ GPIO.setup(trigPin, GPIO.OUT)
 GPIO.setup(echoPin, GPIO.IN)
 GPIO.setup(switch, GPIO.IN)
 
+try:
+    while True:
+        GPIO.output(trigPin, GPIO.LOW)
+        time.sleep(2)
+        GPIO.output(trigPin, GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(trigPin, GPIO.LOW)
+        while GPIO.input(echoPin) == 0:
+            pulse_start_time = time.time()
+        while GPIO.input(echoPin) == 1:
+            pulse_end_time = time.time()
+        pulse_duration = pulse_end_time - pulse_start_time
+        distance = round(pulse_duration*17150, 2)
+        print ("Distance: ",distance, " cm")
 
-while True:
-    GPIO.output(trigPin, GPIO.LOW)
-    time.sleep(2)
-    GPIO.output(trigPin, GPIO.HIGH)
-    time.sleep(0.00001)
-    GPIO.output(trigPin, GPIO.LOW)
-    while GPIO.input(echoPin) == 0:
-        pulse_start_time = time.time()
-    while GPIO.input(echoPin) == 1:
-        pulse_end_time = time.time()
-    pulse_duration = pulse_end_time - pulse_start_time
-    distance = round(pulse_duration*17150, 2)
-    print ("Distance: ",distance, " cm")
-
-    if GPIO.input(switch) == GPIO.HIGH:
-        GPIO.output(greenLedPin, GPIO.HIGH)
-        GPIO.output(yellowLedPin, GPIO.LOW)
-        if (distance<=5):
-            GPIO.output(greenLedPin, GPIO.LOW)
+        if GPIO.input(switch) == GPIO.HIGH:
+            GPIO.output(greenLedPin, GPIO.HIGH)
             GPIO.output(yellowLedPin, GPIO.LOW)
-            GPIO.output(redLedPin, GPIO.HIGH)
+            if (distance<=5):
+                GPIO.output(greenLedPin, GPIO.LOW)
+                GPIO.output(yellowLedPin, GPIO.LOW)
+                GPIO.output(redLedPin, GPIO.HIGH)
 
-    if GPIO.input(switch) == GPIO.LOW:
-        GPIO.output(greenLedPin, GPIO.LOW)
-        GPIO.output(yellowLedPin, GPIO.HIGH)
-        if (distance<=5):
+        if GPIO.input(switch) == GPIO.LOW:
             GPIO.output(greenLedPin, GPIO.LOW)
-            GPIO.output(yellowLedPin, GPIO.LOW)
-            GPIO.output(redLedPin, GPIO.HIGH)
-
+            GPIO.output(yellowLedPin, GPIO.HIGH)
+            if (distance<=5):
+                GPIO.output(greenLedPin, GPIO.LOW)
+                GPIO.output(yellowLedPin, GPIO.LOW)
+                GPIO.output(redLedPin, GPIO.HIGH)
+except KeyboardInterrupt:
+    print("Measurement Stopped by  User")
     GPIO.cleanup()
